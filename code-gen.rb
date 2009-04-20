573a6ee60f8468507849a5ebd7eb4a46fbbca20a
Your Name <erwaller@gmail.com>
2009-04-19 21:48:26 -0700

    code-gen outputs vhdl which compiles

--- code-gen.rb
+++ code-gen.rb
@@ -2,37 +2,30 @@ require 'erb'
 
 def erb (view)
   template = ERB.new(IO.read("#{view}.erb"))
-  template.result
+  template.result.gsub(/\s+\n/m, "\n")
 end
 
-class Individual
-  def init(inputs, outputs)
-    @inputs = inputs
-    @outputs = outputs
-    @states = []
-    add_state
-  end
-
-  def add_state()
-    @states.push(State.new())
+class Fixnum
+  def to_bv(length)
+    self.to_s(2).rjust(length, '0')
   end
 end
 
-class State
-  def output=(out)
-    
-  end
-  
-  def transitions
-    
-  end
-end
+# a state is an array of transitions, and an output
+# integer entries in the transition table represent
+# next states, and nil entries represent no transition
+# 
+# here's a sample state machine with 2 inputs, 1 output
+# and 2 states.
+#
+# inputs =>  00   01   10   11   | output 
+# s0     => [nil,   0, nil,   1] |   0
+# s1     => [1  , nil,   0,   0] |   1
 
-class StateTransition
-  def
-end
-
-@states = [0, 1, 2, 3]
 @inputs = 2
 @outputs = 1
+@states = [
+  {:transitions => [nil, 0, nil, 1], :output => 0},
+  {:transitions => [1, nil, 0, 0], :output => 1},
+]
 print erb :individual
