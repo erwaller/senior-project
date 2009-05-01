a832d220a5bcefd298b607f9f6d27dd63a946f87
Eric Waller <erwaller@gmail.com>
2009-04-30 22:33:54 -0400

    broke optimization techniques out into it's own file so that only adder specific code is in adder.rb.. and include the Adder module automatically by passing as an arg to the HillClimber constructor

--- adder.rb
+++ adder.rb
@@ -1,48 +1,6 @@
-require 'individual'
-require 'monkey'
+# 2-bit adder experiment
 
-  
-class StochasticOptimization
-  attr_reader :best_fitness, :best_individual, :current_generation
-  
-  def initialize(population_size = 20)
-    @population_size = population_size
-    @current_generation = -1
-    @best_fitness = 0
-    @best_individual = nil
-  
-    # Random initial population
-    @individuals = Array.new(@population_size).fill do |i|
-      # 2-bit adder (no carry-in or carry-out)
-      # 4 bits of input, 3 bits of output
-      # we want 7 distinct outputs, so give it 7 initial states
-      Individual.new('adder', i, 4, 3, 7)
-    end
-    
-    # Call the included module's init function
-    init()
-  end
-end
-  
-
-class HillClimber < StochasticOptimization
-  
-  def iterate()
-    sorted = @individuals.sort_by do |individual|
-      individual.fitness = fitness(individual)
-    end
-
-    @best_individual = sorted.last
-    @best_fitness = @best_individual.fitness
-  
-    next_generation = Array.new(@population_size - 1).fill do |i|
-      mutate(@best_individual)
-    end.push(@best_individual.deep_copy) # bring a direct copy of the best individual
-    
-    @individuals = next_generation
-    @current_generation += 1
-  end
-end
+require 'optimization_techniques'
 
 module Adder
   TEST_CASES = []
@@ -72,12 +30,7 @@ module Adder
   end
 end
 
-
-class HillClimber
-  include Adder
-end
-
-h = HillClimber.new()
+h = HillClimber.new(Adder)
 boundary = 0.4
 while 1 do
   h.iterate()
